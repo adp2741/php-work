3bde64d55c9a07eda375055a8c9217434b9615ef
Andrew Perry <adp2741@gmail.com>
2017-05-12 15:17:52 -0400

    Changed file structure

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -1,8 +1,10 @@
-<!DOCTYPE>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
 <html lang="en">
 	<head>
 		<title>Widget Corp</title>
-		<link href="css/style.css" media="all" rel="stylesheet" type="text/css" />
+		<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
     <div id="header">
