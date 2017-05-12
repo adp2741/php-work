5c30c898a4bfd151751bc1e445e2110ece5a24ba
Andrew Perry <adp2741@gmail.com>
2017-05-12 11:06:39 -0400

    More setup

---
+++ includes/layouts/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE>
+<html lang="en">
+	<head>
+		<title>untitled</title>
+		<link href="css/style.css" media="all" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+    <div id="header">
+      <h1>Widget Corp</h1>
+    </div>
